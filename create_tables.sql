4037b60212df49e49c16e1c65548ac67e0f5ff5d
engeell <engjellmeholli10@gmail.com>
2026-09-25 16:21:13 +0200

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,1 +1,11 @@
-
+CREATE TABLE books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    author TEXT,
+    genre TEXT,
+    publication_year INTEGER,
+    price INTEGER,
+    stock INTEGER,
+    rating INTEGER,
+    language TEXT
+);
